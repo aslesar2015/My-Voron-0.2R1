973986cddf6698fec4df82fd59f6dfda5603c204
printer <biqu@BTT-CB1-Sa8O5jf>
2024-08-19 10:50:59 +0300

    update_mcu.sh modified - 08/19/2024 - 10:50:54 AM

--- printer_data/config/script/update_mcu.sh
+++ printer_data/config/script/update_mcu.sh
@@ -19,36 +19,34 @@ make clean
 make -j4 KCONFIG_CONFIG=/home/biqu/printer_data/config/script/config.ebb36.Katapult
 mv ~/katapult/out/deployer.bin ~/firmware/ebb36_katapult.bin
 
-cd ~/klipper
-make clean
-#make menuconfig KCONFIG_CONFIG=/home/biqu/printer_data/config/script/config.manta_m5p.CAN
-make -j4 KCONFIG_CONFIG=/home/biqu/printer_data/config/script/config.manta_m5p.CAN
-mv ~/klipper/out/klipper.bin ~/firmware/manta_m5p_klipper.bin
+#cd ~/klipper
+#make clean
+##make menuconfig KCONFIG_CONFIG=/home/biqu/printer_data/config/script/config.manta_m5p.CAN
+#make -j4 KCONFIG_CONFIG=/home/biqu/printer_data/config/script/config.manta_m5p.CAN
+#mv ~/klipper/out/klipper.bin ~/firmware/manta_m5p_klipper.bin
 
-make clean
-#make menuconfig KCONFIG_CONFIG=/home/biqu/printer_data/config/script/config.ebb36.CAN
-make -j4 KCONFIG_CONFIG=/home/biqu/printer_data/config/script/config.ebb36.CAN
-mv ~/klipper/out/klipper.bin ~/firmware/ebb36_klipper.bin
+#make clean
+##make menuconfig KCONFIG_CONFIG=/home/biqu/printer_data/config/script/config.ebb36.CAN
+#make -j4 KCONFIG_CONFIG=/home/biqu/printer_data/config/script/config.ebb36.CAN
+#mv ~/klipper/out/klipper.bin ~/firmware/ebb36_klipper.bin
 
-cd ~/katapult/scripts
 ## Update MCU EBB36
 #echo "Start update MCU EBB36"
 #echo ""
-python3 flash_can.py -i can0 -u 59d755ed9e3b -f ~/firmware/ebb36_katapult.bin
-sleep 2
-python3 flash_can.py -i can0 -u 59d755ed9e3b -f ~/firmware/ebb36_klipper.bin
-sleep 2
+python3 ~/katapult/scripts/flashtool.py -i can0 -u 59d755ed9e3b -f ~/firmware/ebb36_katapult.bin
+#sleep 2
+#python3 ~/katapult/scripts/flashtool.py -i can0 -u 59d755ed9e3b -f ~/firmware/ebb36_klipper.bin
 #read -p "MCU EBB36 firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
-#echo "Finish update MCU EBB36"
+echo "Finish update MCU EBB36"
 
 # Update MCU Manta M5P
 echo "Start update MCU Manta M5P"
 echo ""
-python3 flash_can.py -i can0 -u c7b31ed2d4a5 -f ~/firmware/manta_m5p_katapult.bin
+python3 ~/katapult/scripts/flashtool.py -i can0 -u c7b31ed2d4a5 -r
 sleep 2
-python3 flash_can.py -d /dev/serial/by-id/usb-katapult_stm32g0b1xx_08002B000C504B5735313920-if00 -f ~/firmware/manta_m5p_katapult.bin
-sleep 2
-python3 flash_can.py -d /dev/serial/by-id/usb-katapult_stm32g0b1xx_08002B000C504B5735313920-if00 -f ~/firmware/manta_m5p_klipper.bin
+python3 ~/katapult/scripts/flashtool.py -d /dev/serial/by-id/usb-katapult_stm32g0b1xx_08002B000C504B5735313920-if00 -f ~/firmware/manta_m5p_katapult.bin
+#sleep 2
+#python3 ~/katapult/scripts/flashtool.py -d /dev/serial/by-id/usb-katapult_stm32g0b1xx_08002B000C504B5735313920-if00 -f ~/firmware/manta_m5p_klipper.bin
 #read -p "MCU Manta M5P firmware flashed, please check above for any errors. Press [Enter] to continue, or [Ctrl+C] to abort"
 echo "Finish update MCU Manta M5P"
 
